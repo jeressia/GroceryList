5676fa265385be560863aa1bfe3987b423a53eac
Jeressia Williamson <jeressiajaytv@gmail.com>
2019-10-22 13:02:58 -0500

    wip

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
+const milk = document.getElementById('milk');
+const eggs = document.getElementById('eggs');
+const batteries = document.getElementById('batteries');
+const bread = document.getElementById('bread');
+const oj = document.getElementById('oj');
+const gj = document.getElementById('gj');
+const bananas = document.getElementById('bananas');
+const pears = document.getElementById('pears');
+const apples = document.getElementById('apples');
+const amcheese = document.getElementById('amcheese');
+const chedcheese = document.getElementById('chedcheese');
+const shrdcheese = document.getElementById('shrdcheese');
+
 const myList = [
   {
     id: "milk",
@@ -73,8 +86,10 @@ const printToDom = (divId, textToPrint) => {
 const groceryBuilder = () => {
   let domString = "";
   myList.forEach(item => {
-    domString +=  `<li><input type="checkbox" name="${item.name}" id="${item.id}">
-                    <label for="${item.name}">${item.name}</label></li>`
+    domString +=  `<li><input type="checkbox" name="${item.name}" id="${item.id}">`;
+    domString +=  `<label for="${item.name}">${item.name}</label></li>`;
+    const groceryItem = `${item.id}`;
+    console.log(groceryItem);
                     
 })
   printToDom('list',domString)
@@ -84,18 +99,7 @@ const groceryBuilder = () => {
 
 
 const toggleEvent = (event) => {
-  const milk = document.getElementById('milk');
-  const eggs = document.getElementById('eggs');
-  const batteries = document.getElementById('batteries');
-  const bread = document.getElementById('bread');
-  const oj = document.getElementById('oj');
-  const gj = document.getElementById('gj');
-  const bananas = document.getElementById('bananas');
-  const pears = document.getElementById('pears');
-  const apples = document.getElementById('apples');
-  const amcheese = document.getElementById('amcheese');
-  const chedcheese = document.getElementById('chedcheese');
-  const shrdcheese = document.getElementById('shrdcheese');
+
 
   // const checked = event.target.checked
   // if (checked){
@@ -112,17 +116,17 @@ const toggleEvent = (event) => {
 
 const eventListeners = () => {
     const milkEvent = document.getElementById('milk').addEventListener('click', toggleEvent);
-    const eggsEvent = document.getElementById('eggs').addEventListener('click', toggleEvent);
-    const batteriesEvent = document.getElementById('batteries').addEventListener('click', toggleEvent);
-    const breadEvent = document.getElementById('bread').addEventListener('click', toggleEvent);
-    const ojEvent = document.getElementById('oj').addEventListener('click', toggleEvent);
-    const gjEvent = document.getElementById('gj').addEventListener('click', toggleEvent);
-    const bananasEvent = document.getElementById('bananas').addEventListener('click', toggleEvent);
-    const pearsEvent = document.getElementById('pears').addEventListener('click', toggleEvent);
-    const applesEvent = document.getElementById('apples').addEventListener('click', toggleEvent);
-    const amcheeseEvent = document.getElementById('amcheese').addEventListener('click', toggleEvent);
-    const chedcheeseEvent = document.getElementById('chedcheese').addEventListener('click', toggleEvent);
-    const shrdcheeseEvent = document.getElementById('shrdcheese').addEventListener('click', toggleEvent);
+    // const eggsEvent = document.getElementById('eggs').addEventListener('click', toggleEvent);
+    // const batteriesEvent = document.getElementById('batteries').addEventListener('click', toggleEvent);
+    // const breadEvent = document.getElementById('bread').addEventListener('click', toggleEvent);
+    // const ojEvent = document.getElementById('oj').addEventListener('click', toggleEvent);
+    // const gjEvent = document.getElementById('gj').addEventListener('click', toggleEvent);
+    // const bananasEvent = document.getElementById('bananas').addEventListener('click', toggleEvent);
+    // const pearsEvent = document.getElementById('pears').addEventListener('click', toggleEvent);
+    // const applesEvent = document.getElementById('apples').addEventListener('click', toggleEvent);
+    // const amcheeseEvent = document.getElementById('amcheese').addEventListener('click', toggleEvent);
+    // const chedcheeseEvent = document.getElementById('chedcheese').addEventListener('click', toggleEvent);
+    // const shrdcheeseEvent = document.getElementById('shrdcheese').addEventListener('click', toggleEvent);
 }
 
 const init = () => {
